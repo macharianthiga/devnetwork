630d2bf5920a0f71a01ef8fd78f1ef82e9e01fe6
macharianthiga <dmdmasha28@gmail.com>
2018-09-24 15:18:52 +0300

    create profile model and import it in the profile route

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router  = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load profile  and user models
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route     GET api/profiles/test
 // @desc      Tests profile route
 // @access    Public
 router.get('/test', (req, res) => res.json({msg: "Profiles Works"}));
+
+
+// @route     GET api/profiles/test
+// @desc      Tests profile route
+// @access    Public
 
 module.exports = router;
